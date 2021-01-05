11f2f4288b716561a09055bf9ae42bf5e16ca0d2
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2021-01-05 17:43:34 +0300

    get ip

--- generate_link.php
+++ generate_link.php
@@ -1,33 +1,45 @@
 <html>
- <head>
-  <title>Тестируем PHP</title>
- </head>
- <body>
-   <!DOCTYPE html>
- <html>
- <body>
+    <head>
+        <title>Тестируем PHP</title>
+    </head>
+    <body>
 
- <?php
-   $id = rand(1000, 9999);
-   echo strval($id);
-   $dir = strval($id);
+        <?php
+            $id = rand(1000000, 9999999);
+            echo strval($id);
+            $dir = strval($id);
 
-   if( is_dir($dir) === false ){
-     mkdir($dir);
-   }
+            if( is_dir($dir) === false ){
+             mkdir($dir);
+            }
 
-   $file_to_write = "index.html";
-   $content_to_write = "<!DOCTYPE html><html><head><meta><title></title></head><body><p>you are gay!</p></body></html>";
+            $file_to_write = "index.html";
+            $content_to_write = "<!DOCTYPE html>
+                                <html>
+                                <head>
+                                <meta>
+                                <title></title>
+                                <script type=\"text/javascript\" src=\"../jquery.js\"></script>
+                                </head>
+                                <body>
+                                <p>you are gay!</p>
+                                <script>
+                                $.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+                                    console.log(JSON.stringify(data, null, 2));
+                                    ip = JSON.parse(JSON.stringify(data, null, 2));
+                                    console.log(ip[\"geobytesipaddress\"]);
+                                });
+                                </script></body></html>";
 
-   $file = fopen($dir . '/' . $file_to_write,"w");
+            $file = fopen($dir . '/' . $file_to_write,"w");
 
-   // a different way to write content into
-   // fwrite($file,"Hello World.");
+            // a different way to write content into
+            // fwrite($file,"Hello World.");
 
-   fwrite($file, $content_to_write);
+            fwrite($file, $content_to_write);
 
-   // closes the file
-   fclose($file);
- ?>
- </body>
+            // closes the file
+            fclose($file);
+        ?>
+    </body>
 </html>
